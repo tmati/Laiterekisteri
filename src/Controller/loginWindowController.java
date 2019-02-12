83b20f171aa946468ad14ae95654ebf5e6b62e80
Jukka Holopainen <jukka.holopainen3@metropolia.fi>
2019-02-12 12:35:37 +0200

    Lisätty salasana encryptaus ensimmäinen kohta tehty.

--- src/Controller/loginWindowController.java
+++ src/Controller/loginWindowController.java
@@ -7,6 +7,8 @@ package Controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -206,5 +208,29 @@ public class loginWindowController implements Initializable {
             loginInactive.setDisable(false);
             loginPossible = false;
         }
-    } 
+    }
+    public String passwordConverter(String password){
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(password.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            password = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return password;
+    }
 }
