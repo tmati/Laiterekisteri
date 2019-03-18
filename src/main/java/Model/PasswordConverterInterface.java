d2dda628571ec35d5f19e97ed45b10932690000a
Jukka Holopainen <jukka.holopainen3@metropolia.fi>
2019-03-18 18:11:17 +0200

    Lisäsin pari luokaa modeliin (PasswordConverterin ja sen interfasin) ja muookasin modelia. Modelista importasin kaikki luokat ja lisäsin salasanan vaihto medotin salasanan encryptamiseen. T: Jukka Holopainen

---
+++ src/main/java/Model/PasswordConverterInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ * Salasanan kääntäjän interface
+ * @author jukka
+ */
+public interface PasswordConverterInterface {
+
+    /**
+     * Metodi joka encryptaa salasanan.
+     * @param password Salasana joka muutetaan
+     * @return salasanan muutetussa muodossa.
+     */
+    public String passwordConverter(String password);
+}
